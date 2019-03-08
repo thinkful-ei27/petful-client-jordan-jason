418b0be23b6fe24ab840cd4009ba90cbf76c7242
Publikwerker <jason.michael.hoffman@gmail.com>
2019-03-08 09:15:32 -0800

    began dashboard component

---
+++ src/dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function Dashboard(props){
+
+  // const catSection = () => {
+  //   return (
+  //     <div>
+  //       {this.props.catToAdopt.name}
+  //     </div>
+  //   )
+  // }
+
+  // const dogSection = () => {
+  //   return (
+  //     <div>
+  //       {this.state.dogToAdopt.name}
+  //     </div>
+  //   )
+  // }
+
+  render(){
+    return(
+      <div>
+        <p>this is the stuff</p>
+        {/* {catSection}
+        {dogSection} */}
+      </div>
+    )
+
+  }
+
+
+
+
+}
