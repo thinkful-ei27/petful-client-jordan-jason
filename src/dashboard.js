e7a0eec6668c0ba59bd93708a7d9f6080d12486c
Jordan Heffernan <jheff2@gmail.com>
2019-03-08 09:43:18 -0800

    react app up and running with dummy data hardcoded

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,35 +1,55 @@
 import React from 'react';
 
-export default function Dashboard(props){
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.catToAdpot = props.catToAdpot;
+    this.dogToAdopt = props.dogToAdopt;
+  }
 
-  // const catSection = () => {
-  //   return (
-  //     <div>
-  //       {this.props.catToAdopt.name}
-  //     </div>
-  //   )
-  // }
-
-  // const dogSection = () => {
-  //   return (
-  //     <div>
-  //       {this.state.dogToAdopt.name}
-  //     </div>
-  //   )
-  // }
-
-  render(){
-    return(
-      <div>
-        <p>this is the stuff</p>
-        {/* {catSection}
-        {dogSection} */}
+  render() {
+    return (
+      <div className='pet-info' >
+        <div className='cat-info'>
+          <header>
+            <h2>{this.catToAdpot.name}</h2>
+            <img src={this.catToAdpot.imageURL} alt={this.catToAdpot.imageDescription} />
+          </header>
+          <main>
+            <dl>
+              <dt>Gender</dt>
+              <dd>{this.catToAdpot.sex}</dd>
+              <dt>Age</dt>
+              <dd>{this.catToAdpot.age}</dd>
+              <dt>Breed</dt>
+              <dd>{this.catToAdpot.breed}</dd>
+              <dt>Story</dt>
+              <dd>{this.catToAdpot.story}</dd>
+            </dl>
+            <button>Adopt</button>
+          </main>
+        </div>
+        <div className='dog-info'>
+          <header>
+            <h2>{this.dogToAdopt.name}</h2>
+            <img src={this.dogToAdopt.imageURL} alt={this.dogToAdopt.imageDescription} />
+          </header>
+          <main>
+            <dl>
+              <dt>Gender</dt>
+              <dd>{this.dogToAdopt.sex}</dd>
+              <dt>Age</dt>
+              <dd>{this.dogToAdopt.age}</dd>
+              <dt>Breed</dt>
+              <dd>{this.dogToAdopt.breed}</dd>
+              <dt>Story</dt>
+              <dd>{this.dogToAdopt.story}</dd>
+            </dl>
+            <button>Adopt</button>
+          </main>
+        </div>
       </div>
     )
 
   }
-
-
-
-
 }
